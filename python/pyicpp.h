6b40a08c0fd316cff03a8dfb2ab08bf1791c5e42
George Hilley <gehilley@gmail.com>
2019-04-16 12:55:27 -0700

    cleanup

--- python/pyicpp.h
+++ python/pyicpp.h
@@ -1,8 +1,8 @@
-#ifndef PYICP_H
-#define PYICP_H
+#ifndef PYICPP_H
+#define PYICPP_H
 
 #include "icp.h"
 
 double pyicp(double *fixed, int num_fixed, double *moving, int num_moving, double *rotation, double *translation);
 
-#endif // PYICP_H
+#endif // PYICPP_H
