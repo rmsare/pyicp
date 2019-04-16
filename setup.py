b70cb87482b0576e20b9083a388d0855d66403b0
George Hilley <gehilley@gmail.com>
2019-04-16 13:05:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,75 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy as np
-import os
-import platform
+from setuptools.command.build_ext import build_ext
 
-os.environ["CC"]= "/usr/bin/g++"
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', **kwa):
+        Extension.__init__(self, name, sources=[], **kwa)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
 
-if platform.system() == 'Darwin':
-    extra_compile_args = ["-std=c++03"]
-    extra_link_args = ["-std=c++03"]
-else:
-    extra_compile_args = []
-    extra_link_args = []
+class cmake_build_ext(build_ext):
+    def build_extensions(self):
+        # Ensure that CMake is present and working
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
 
-setup(
-    name='pyicp',
-    ext_modules=cythonize(
-        Extension(
-            "pyicp",
-            sources=["python/pyicp.pyx", "python/pyicpp.cpp"],
-            include_dirs=[np.get_include(), "api/"],
-	    language="c++",
-            extra_compile_args=extra_compile_args+['-L./lib/', '-licp'],
-            extra_link_args=extra_link_args+['-L./lib/', '-licp']
-        )
-    ),
-    install_requires=["numpy"]
-)
+        for ext in self.extensions:
+
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            cfg = 'Debug' if options['--debug'] == 'ON' else 'Release'
+
+            cmake_args = [
+                '-DCMAKE_BUILD_TYPE=%s' % cfg,
+                # Ask CMake to place the resulting library in the directory
+                # containing the extension
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                # Other intermediate static libraries are placed in a
+                # temporary build directory instead
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                # Hint CMake to use the same Python executable that
+                # is launching the build, prevents possible mismatching if
+                # multiple versions of Python are installed
+                '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+                # Add other project-specific CMake arguments if needed
+                # ...
+            ]
+
+            # We can handle some platform-specific settings at our discretion
+            if platform.system() == 'Windows':
+                plat = ('x64' if platform.architecture()[0] == '64bit' else 'Win32')
+                cmake_args += [
+                    # These options are likely to be needed under Windows
+                    '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE',
+                    '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                ]
+                # Assuming that Visual Studio and MinGW are supported compilers
+                if self.compiler.compiler_type == 'msvc':
+                    cmake_args += [
+                        '-DCMAKE_GENERATOR_PLATFORM=%s' % plat,
+                    ]
+                else:
+                    cmake_args += [
+                        '-G', 'MinGW Makefiles',
+                    ]
+
+            cmake_args += cmake_cmd_args
+
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            # Config
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+
+            # Build
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg],
+                                  cwd=self.build_temp)
+
+
+setup(name = "pyicp",
+      version = "1.0",
+      ext_modules = [CMakeExtension("pyicp")]
+      cmdclass = {'build_ext': cmake_build_ext},
+     );
 
