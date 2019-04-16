d9e88fb07c0b7c4994c3a026a9b4b77752ac701b
George Hilley <gehilley@gmail.com>
2019-04-16 09:38:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 import os
+import platform
 
 os.environ["CC"]= "/usr/bin/g++"
+
+if platform.system() == 'Darwin':
+    extra_compile_args = ["-std=c++03"]
+    extra_link_args = ["-std=c++03"]
+else:
+    extra_compile_args = []
+    extra_link_args = []
 
 setup(
     name='pyicp',
@@ -13,8 +21,8 @@ setup(
             sources=["python/pyicp.pyx", "python/pyicpp.cpp"],
             include_dirs=[np.get_include(), "api/"],
 	    language="c++",
-            extra_compile_args=["-std=c++03", "-mmacosx-version-min=10.9", '-L./lib/', '-licp'],
-            extra_link_args=["-std=c++03", "-mmacosx-version-min=10.9", '-L./lib/', '-licp']
+            extra_compile_args=extra_compile_args+['-L./lib/', '-licp'],
+            extra_link_args=extra_link_args+['-L./lib/', '-licp']
         )
     ),
     install_requires=["numpy"]
