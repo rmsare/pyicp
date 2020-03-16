9d9e9bbff6a0735009c33cd732ab7adca208515d
George Hilley <gehilley@gmail.com>
2020-03-16 06:16:54 -0700

    fixed naming collisions with pyicp

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-def __bootstrap__():
-   global __bootstrap__, __loader__, __file__
-   import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,'pyicp.so')
-   __loader__ = None; del __bootstrap__, __loader__
-   imp.load_dynamic(__name__,__file__)
-__bootstrap__()
+import numpy as np
+import copy, imp, os
 
+pylibicp = imp.load_dynamic("pylibicp",os.path.join(__path__[0],"python/pylibicp.so"))
 
+def icp(fixed , moving):
+   assert(isinstance(fixed,np.ndarray) and isinstance(moving,np.ndarray))
+   assert(fixed.shape[1] == 3 and moving.shape[1] == 3)
+   from pylibicp import icp_interface
+   fixed = np.unique(copy.deepcopy(fixed), axis=0)
+   moving = np.unique(copy.deepcopy(moving),axis=0)
+   return icp_interface(fixed,moving)
---
+++ python/pylibicp.pyx
@@ -0,0 +1,31 @@
+import numpy as np
+import copy
+cimport pylibicp
+cimport numpy as np
+from libc.stdlib cimport malloc, free
+
+def icp_interface(np.ndarray[double, ndim = 2, mode = 'c'] fixed not None, np.ndarray[double, ndim = 2, mode = 'c'] moving not None):
+
+  m_fixed, n_fixed = fixed.shape[0], fixed.shape[1]
+  m_moving, n_moving = moving.shape[0], moving.shape[1]
+
+  assert n_fixed == 3
+  assert n_moving == 3
+
+  cdef double *rotation = <double *> malloc(9*sizeof(double))
+  cdef double *translation = <double *> malloc(3*sizeof(double))
+  cdef double resid
+
+  with nogil:
+     resid = pyicp(&fixed[0,0], m_fixed, &moving[0,0], m_moving, rotation, translation);
+
+  transform = np.array([[rotation[0], rotation[1], rotation[2], translation[0]],[rotation[3], rotation[4], rotation[5], translation[1]],[rotation[6], rotation[7], rotation[8], translation[2]],[0.0, 0.0, 0.0, 1.0]])
+
+  residual = np.array([resid])
+
+  free(rotation)
+  free(translation)
+
+  return transform, residual
+
+
